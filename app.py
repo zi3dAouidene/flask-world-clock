2da20e37f1bbb8c481cd5dfa40053252e34c11e0
Eric Hu <huericnan@gmail.com>
2022-09-23 15:35:58 -0400

    context logging

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 import requests
-import logging
-import json
-from flask import Flask, request, render_template
+import uuid
+from flask import Flask, request, render_template, session
 from logging.config import dictConfig
 
-
-class JSONFormatter(logging.Formatter):
-	def __init__(self):
-		super().__init__()
-	def format(self, record):
-		record.msg = json.dumps(record.msg)
-		return super().format(record)
 
 dictConfig(
     {
@@ -19,7 +11,7 @@ dictConfig(
             "default": {
                 "format": "[%(asctime)s] [%(levelname)s | %(module)s] %(message)s",
                 "datefmt": "%B %d, %Y %H:%M:%S %Z",
-            }
+            },
         },
         "handlers": {
             "console": {
@@ -34,7 +26,7 @@ dictConfig(
             "logtail": {
                 "class": "logtail.LogtailHandler",
                 "source_token": "qU73jvQjZrNFHimZo4miLdxF",
-                "formatter": JSONFormatter(),
+                "formatter": "default",
             },
         },
         "root": {"level": "DEBUG", "handlers": ["console", "file", "logtail"]},
@@ -43,11 +35,15 @@ dictConfig(
 
 app = Flask(__name__)
 
+app.secret_key = "c00de22a8b1e4daa2cabc8b3f82fdb753574293f8b673f9a"
+
 
 @app.route("/")
 def home():
 
-    app.logger.info("A user visited the home page")
+    session["ctx"] = {"request_id": str(uuid.uuid4())}
+
+    app.logger.info("A user visited the home page >>> %s", session["ctx"])
 
     return render_template("home.html")
 
@@ -58,7 +54,9 @@ def search():
     # Get the search query
     query = request.form["q"]
 
-    app.logger.info("A user performed a search. >>> %s", query)
+    app.logger.info(
+        "A user performed a search. | query: %s >>> %s", query, session["ctx"]
+    )
 
     # Pass the search query to the Nominatim API to get a location
     location = requests.get(
@@ -69,7 +67,9 @@ def search():
     # If a location is found, pass the coordinate to the Time API to get the current time
     if location:
 
-        app.logger.info("A location is found. >>> %s", location)
+        app.logger.info(
+            "A location is found. | location: %s >>> %s", location, session["ctx"]
+        )
 
         coordinate = [location[0]["lat"], location[0]["lon"]]
 
@@ -83,6 +83,21 @@ def search():
     # If a location is NOT found, return the error page
     else:
 
-        app.logger.info("A location is NOT found.")
+        app.logger.info("A location is NOT found. >>> %s", session["ctx"])
 
         return render_template("fail.html")
+
+
+@app.after_request
+def logAfterRequest(response):
+
+    app.logger.info(
+        "path: %s | method: %s | status: %s | size: %s >>> %s",
+        request.path,
+        request.method,
+        response.status,
+        response.content_length,
+        session["ctx"],
+    )
+
+    return response
