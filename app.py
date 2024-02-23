4d4db11a0df72071834ea09ac6c27fe1151407e5
Zied AOUIDENE <zied.aouidene@gmail.com>
2024-02-23 17:51:28 +0100

    metrics

--- app.py
+++ app.py
@@ -1,39 +1,52 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import requests
+import time
+from prometheus_client import start_http_server, Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+SEARCH_REQUESTS_TOTAL = Counter('search_requests_total', 'Total number of search requests.')
+SEARCH_REQUESTS_DURATION = Histogram('search_request_duration_seconds', 'Duration of /search requests in seconds.')
+
+def record_search_duration(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        duration = time.time() - start_time
+        SEARCH_REQUESTS_DURATION.observe(duration)
+        return result
+    return wrapper
 
 @app.route("/")
 def home():
-
     return render_template("home.html")
 
-
 @app.route("/search", methods=["POST"])
+@record_search_duration
 def search():
-
-    # Get the search query
+    SEARCH_REQUESTS_TOTAL.inc()
     query = request.form["q"]
-
-    # Pass the search query to the Nominatim API to get a location
     location = requests.get(
         "https://nominatim.openstreetmap.org/search",
-        {"q": query, "format": "json", "limit": "1"},
+        params={"q": query, "format": "json", "limit": "1"},
     ).json()
 
-    # If a location is found, pass the coordinate to the Time API to get the current time
     if location:
         coordinate = [location[0]["lat"], location[0]["lon"]]
-
-        time = requests.get(
+        time_data = requests.get(
             "https://timeapi.io/api/Time/current/coordinate",
-            {"latitude": coordinate[0], "longitude": coordinate[1]},
-        )
+            params={"latitude": coordinate[0], "longitude": coordinate[1]},
+        ).json()
 
-        return render_template("success.html", location=location[0], time=time.json())
-
-    # If a location is NOT found, return the error page
+        return render_template("success.html", location=location[0], time=time_data)
     else:
-
         return render_template("fail.html")
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+if __name__ == '__main__':
+
+    # Run the Flask application
+    app.run('0.0.0.0',debug=True)
